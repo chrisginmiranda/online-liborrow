f078d1be46a446bc49a86927888ab36f88a4baee
chrisginmiranda <106379236+chrisginmiranda@users.noreply.github.com>
2022-05-28 00:07:31 +0800

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "onlineliborrow";
+
+// Create connection
+$conn = mysqli_connect($server, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
